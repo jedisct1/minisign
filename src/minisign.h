0efc6942b120389172f40ae9a73580d01d2c4479
Frank Denis <github@pureftpd.org>
2015-11-11 11:25:14 +0100

    0.6

--- src/minisign.h
+++ src/minisign.h
@@ -17,7 +17,7 @@
 #define SIG_DEFAULT_PKFILE "minisign.pub";
 #define SIG_DEFAULT_SKFILE "minisign.key";
 #define SIG_SUFFIX ".minisig"
-#define VERSION_STRING "minisign 0.5"
+#define VERSION_STRING "minisign 0.6"
 
 typedef struct KeynumSK_ {
     unsigned char keynum[KEYNUMBYTES];
