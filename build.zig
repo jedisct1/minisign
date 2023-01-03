7700b267740440df2f35299f431c91aa0db52b91
overcat <4catcode@gmail.com>
2023-01-03 20:00:13 +0800

    Update `build.zig` to be compatible with zig 0.10.0+

--- build.zig
+++ build.zig
@@ -9,13 +9,13 @@ pub fn build(b: *std.build.Builder) !void {
     minisign.setBuildMode(mode);
     minisign.install();
     minisign.linkLibC();
-    minisign.addLibPath("/opt/homebrew/lib");
-    minisign.addLibPath("/usr/local/lib");
+    minisign.addLibraryPath("/opt/homebrew/lib");
+    minisign.addLibraryPath("/usr/local/lib");
     minisign.linkSystemLibrary("sodium");
 
-    minisign.addIncludeDir("src");
-    minisign.addSystemIncludeDir("/opt/homebrew/include");
-    minisign.addSystemIncludeDir("/usr/local/include");
+    minisign.addIncludePath("src");
+    minisign.addSystemIncludePath("/opt/homebrew/include");
+    minisign.addSystemIncludePath("/usr/local/include");
     minisign.defineCMacro("_GNU_SOURCE", "1");
     minisign.addCSourceFiles(&.{ "src/base64.c", "src/get_line.c", "src/helpers.c", "src/minisign.c" }, &.{});
 }
